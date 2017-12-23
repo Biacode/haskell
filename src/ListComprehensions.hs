41585a8ac793fd737a20204eac564bf5c0a6ea0a
Arthur Asatryan <biacoder@gmail.com>
2017-12-23 19:23:58 +0400

    Advanced list comprehensions

---
+++ src/ListComprehensions.hs
@@ -0,0 +1,23 @@
+module ListComprehensions where
+
+listComprehension = [ x | x <- [50..100], x `mod` 7 == 3]
+
+boomBangs li = [if x < 50 then "Boom!!" else "Bang!!" | x <- li, odd x]
+
+multiplePredicateComprehension = [ x | x <- [10..20], x /= 13, x /= 15, x /= 19]
+
+multiListComprehension'decardeanProduct = [x * y | x <- [1..10], y <- [13..22], x * y > 50]
+
+nouns = ["hobo","frog","pope"]
+
+adjectives = ["lazy","grouchy","scheming"]
+
+combinationOfNounsAndAdjectives = [adjective ++ " " ++ noun | adjective <- adjectives, noun <- nouns]
+
+length' xs = sum [1 | _ <- xs]
+
+removeNonUppercase st = [c | c <- st, c `elem` ['A'..'Z']]
+
+xxs = [[1,3,5,2,3,1,2,4,5],[1,2,3,4,5,6,7,8,9],[1,2,4,2,1,6,3,1,3,2,3,6]]
+
+removeOddsWithoutFlatten = [[x | x <- xs, even x] | xs <- xxs]
