bb985d5958873c66c32ca4ca9c6cd5b70504acb5
Arthur Asatryan <biacoder@gmail.com>
2017-12-23 17:37:47 +0400

    List comprehensions

--- src/Baby.hs
+++ src/Baby.hs
@@ -23,3 +23,11 @@ rangeOfAlphabetNumbers from to = [from..to]
 numberRangeWithSomeJump = [0.1, 0.3 .. 1]
 
 firstMultipliesOfInfinite num = take num [2, 4..]
+
+listComprehension = [ x | x <- [50..100], x `mod` 7 == 3]
+
+boomBangs li = [if x < 50 then "Boom!!" else "Bang!!" | x <- li, odd x]
+
+multiplePredicateComprehension = [ x | x <- [10..20], x /= 13, x /= 15, x /= 19]
+
+multiListComprehension'decardeanProduct = [x * y | x <- [1..10], y <- [13..22], x * y > 50]
